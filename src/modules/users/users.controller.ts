cf01320f1025b5e914447b3ef95190740ffae1ab
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-07-04 23:08:59 -0300

    Guard autenticador

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get('/me')
+  @UseGuards(AuthGuard)
+  me(@Req() request: any) {
+    console.log(request.userId);
+    return this.usersService.getUserById('userid');
+  }
 }
