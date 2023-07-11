d3e1e5d0bb0e50410b9734ca465ad27ced58ca58
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-07-10 22:33:59 -0300

    Ajuste na rota para jwt service e rotas privadas

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,15 +1,13 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { AuthGuard } from '../auth/auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('/me')
-  @UseGuards(AuthGuard)
   me(@Req() request: any) {
     console.log(request.userId);
-    return this.usersService.getUserById('userid');
+    return this.usersService.getUserById(request.userId);
   }
 }
