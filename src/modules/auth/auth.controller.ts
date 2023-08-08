8754497f621c8303352bd64939c3ce635138f737
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-08-07 23:07:00 -0300

    Transformando a rota de cadastro e login publicas

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
@@ -8,12 +8,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  signIn(@Body() signInDto: SignInDto) {
+  @SetMetadata('IS_PUBLIC', true)
+  signin(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
   @Post('signup')
-  create(@Body() signUpDto: SignUpDto) {
+  @SetMetadata('IS_PUBLIC', true)
+  signup(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
 }
