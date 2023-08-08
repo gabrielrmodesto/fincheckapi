bbb9dcfb6b9bde52cac6b60f08a98bd272cc16d0
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2023-08-07 23:15:59 -0300

    Criando um decorator para rota publica

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,20 +1,20 @@
-import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signin.dto';
 import { SignUpDto } from './dto/signup.dto';
+import { IsPublic } from '../../shared/decorators/isPublic';
 
+@IsPublic()
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signin')
-  @SetMetadata('IS_PUBLIC', true)
   signin(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
   }
 
   @Post('signup')
-  @SetMetadata('IS_PUBLIC', true)
   signup(@Body() signUpDto: SignUpDto) {
     return this.authService.signUp(signUpDto);
   }
